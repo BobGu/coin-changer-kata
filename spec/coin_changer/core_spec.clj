83a57c40376f118513c0bf643e8fff8c52b10672
bobgu <robertgu84@gmail.com>
2015-12-08 15:17:01 -0800

    can return one quarter if 25 are changed

--- spec/coin_changer/core_spec.clj
+++ spec/coin_changer/core_spec.clj
@@ -3,7 +3,10 @@
             [coin-changer.core :refer :all]))
 
 (describe "coin-changer.core"
-  
+
+  (it "can take 25 cents and return a quarter"
+    (should= "1 quarter(s)" (change-coins 25)))
+
 )
 
 (run-specs)
--- src/coin_changer/core.clj
+++ src/coin_changer/core.clj
@@ -1,1 +1,4 @@
 (ns coin-changer.core)
+(defn change-coins
+  [pennies]
+  (str (/ pennies 25) " quarter(s)"))
