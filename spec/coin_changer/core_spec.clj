c37c215308d544df448f68804e844d4d01fe8a2d
bobgu <robertgu84@gmail.com>
2015-12-09 14:02:33 -0800

    started from scratch, binded a list of coins to coin-types

--- spec/coin_changer/core_spec.clj
+++ spec/coin_changer/core_spec.clj
@@ -2,51 +2,10 @@
   (:require [speclj.core :refer :all]
             [coin-changer.core :refer :all]))
 
-(describe "amount-of-quarters"
+(describe "coins"
 
-  (it "take 25 cents and return 1"
-    (should= 1 (amount-of-quarters 25)))
-
-  (it "takes 24 cents and return 0"
-    (should= 0 (amount-of-quarters 24))))
-
-(describe "amount-of-dimes"
-
-  (it "takes 20 cents and return 2"
-    (should= 2 (amount-of-dimes 20)))
-
-  (it "takes 19 cents and return 1"
-    (should= 1 (amount-of-dimes 19)))
-
-  (it "takes 9 cents and return 0"
-    (should= 0 (amount-of-dimes 9))))
-
-(describe "amount-of-nickels"
-  (it "takes 9 cents and returns 1"
-    (should= 1 (amount-of-nickels 9))))
-
-(describe "change-after-finding-quarters"
-
-  (it "takes 26 cents and return 1"
-    (should= 1 (change-after-finding-quarters 26))))
-
-(describe "change-after-finding-dimes"
-  (it "takes 17 cents and return 7"
-    (should= 7 (change-after-finding-dimes 17))))
-
-(describe "change-coins"
-
-  (it "takes 35 cents and returns 1 quarter 1 dime"
-    (should= "1 quarter(s) 1 dime(s) 0 nickel(s) 0 pennies" (change-coins 35)))
-
-  (it "takes 40 cents and returns 1 quarter 1 dime and 1 nickel"
-    (should= "1 quarter(s) 1 dime(s) 1 nickel(s) 0 pennies" (change-coins 40)))
-
-  (it "takes 44 cents and returns 1 quarter 1 dime and 1 nickels and 4 pennies"
-    (should= "1 quarter(s) 1 dime(s) 1 nickel(s) 4 pennies" (change-coins 44)))
-
-  (it "takes 0 cents and returns 0 quarters dimes nickels and pennies"
-    (should= "0 quarter(s) 0 dime(s) 0 nickel(s) 0 pennies" (change-coins 0))))
+  (it "should give me a list of coins"
+    (should= ["quarter" "dime" "nickel" "penny"] coin-types)))
 
 
 (run-specs)
--- src/coin_changer/core.clj
+++ src/coin_changer/core.clj
@@ -1,34 +1,4 @@
 (ns coin-changer.core)
-(defn amount-of-quarters
-  [pennies]
-  (int (Math/floor (/ pennies 25))))
 
-(defn amount-of-dimes
-  [pennies]
-  (int (Math/floor (/ pennies 10))))
-
-(defn amount-of-nickels
-  [pennies]
-  (int (Math/floor (/ pennies 5))))
-
-(defn change-after-finding-quarters
-  [change]
-  (- change (* 25 (amount-of-quarters change))))
-
-(defn change-after-finding-dimes
-  [change]
-  (-
-    (change-after-finding-quarters change)
-    (* 10 (amount-of-dimes (change-after-finding-quarters change)))))
-
-(defn remaining-pennies
-  [pennies]
-  (mod pennies 5))
-
-(defn change-coins
-  [pennies]
-  (str
-    (amount-of-quarters pennies) " quarter(s) "
-    (amount-of-dimes (change-after-finding-quarters pennies)) " dime(s) "
-    (amount-of-nickels (change-after-finding-dimes pennies))" nickel(s) "
-    (remaining-pennies pennies) " pennies"))
+(def coin-types
+  ["quarter" "dime" "nickel" "penny"])
