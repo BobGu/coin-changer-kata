8be8617e699e0abdb27b88e94c1e4e4a37c54d04
bobgu <robertgu84@gmail.com>
2015-12-08 16:50:16 -0800

    find amout of pennies left over after finding the amount of quarters

--- spec/coin_changer/core_spec.clj
+++ spec/coin_changer/core_spec.clj
@@ -4,21 +4,26 @@
 
 (describe "amount-of-quarters"
 
-  (it "can take 25 cents and return 1"
+  (it "take 25 cents and return 1"
     (should= 1 (amount-of-quarters 25)))
 
-  (it "can take 24 cents and return 0"
+  (it "takes 24 cents and return 0"
     (should= 0 (amount-of-quarters 24))))
 
 (describe "amount-of-dimes"
 
-  (it "can take 20 cents and return 2"
+  (it "takes 20 cents and return 2"
     (should= 2 (amount-of-dimes 20)))
 
-  (it "can take 19 cents and return 1"
+  (it "takes 19 cents and return 1"
     (should= 1 (amount-of-dimes 19)))
 
-  (it "can take 9 cents and return 0"
+  (it "takes 9 cents and return 0"
     (should= 0 (amount-of-dimes 9))))
+
+(describe "pennies-after-finding-quarters"
+
+  (it "takes 26 cents and return 1"
+    (should= 1 (pennies-after-finding-quarters 26))))
 
 (run-specs)
--- src/coin_changer/core.clj
+++ src/coin_changer/core.clj
@@ -6,3 +6,7 @@
 (defn amount-of-dimes
   [pennies]
   (int (Math/floor (/ pennies 10))))
+
+(defn pennies-after-finding-quarters
+  [pennies]
+  (- pennies (* 25 (amount-of-quarters pennies))))
