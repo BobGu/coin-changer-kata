ad8a969b8f6cd45e7a38e0c11f12410afb4168a3
bobgu <robertgu84@gmail.com>
2015-12-08 16:19:36 -0800

    can return amount of times given an amount of pennies

--- spec/coin_changer/core_spec.clj
+++ spec/coin_changer/core_spec.clj
@@ -2,11 +2,14 @@
   (:require [speclj.core :refer :all]
             [coin-changer.core :refer :all]))
 
-(describe "coin-changer.core"
+(describe "amount-of-quarters"
 
-  (it "can take 25 cents and return a quarter"
-    (should= 1 (amount-of-quarters 25)))
+  (it "can take 25 cents and return 1"
+    (should= 1 (amount-of-quarters 25))))
 
-)
+(describe "amount-of-dimes"
+
+  (it "can take 20 cents and return 2"
+    (should= 2 (amount-of-dimes 20))))
 
 (run-specs)
--- src/coin_changer/core.clj
+++ src/coin_changer/core.clj
@@ -2,3 +2,7 @@
 (defn amount-of-quarters
   [pennies]
   (int (Math/floor (/ pennies 25))))
+
+(defn amount-of-dimes
+  [pennies]
+  (int (Math/floor (/ pennies 10))))
