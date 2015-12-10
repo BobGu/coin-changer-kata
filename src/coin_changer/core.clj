4e499f637b7b560ed6169839bbb002f49193aa1e
bobgu <robertgu84@gmail.com>
2015-12-09 17:57:42 -0800

    don helped me

--- src/coin_changer/core.clj
+++ src/coin_changer/core.clj
@@ -3,15 +3,6 @@
 (def coin-type-and-value
   {:quarter 25 :dime 10 :nickel 5 :penny 1})
 
-(defn coin-value
-  [coin-type]
-  (get coin-type-and-value coin-type))
-
 (defn make-change?
   [coin-type remaining-change]
-  (if (< (coin-value coin-type) remaining-change) true false))
-
-; (defn coin-changer
-;   [change-left current-coin-type]
-;   (if (> change-left 0 )
-;     (if (> current-denomination change-left))))
+  (if (< (coin-type coin-type-and-value) remaining-change) true false))
